780f228ad5a8171bd5eaf0a0ca2c1843279c35f6
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-12-02 19:14:32 -0800

    Add font libraries and update name styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        orbitron: ['"Orbitron"', "sans-serif"],
+        tangerine: ["Tangerine", "sans-serif"]
+      }, 
+    },
   },
   variants: {
     extend: {},
